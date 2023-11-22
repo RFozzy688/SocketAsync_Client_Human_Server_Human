33852b3f9b5b1d417ca3149bd4485e1dfd62f5b4
RFozzy688 <pokerfozzy@gmail.com>
2023-11-22 08:40:19 +0200

    верстка

--- SocketAsync_Client_Human_Server_Human/MainWindow.xaml.cs
+++ SocketAsync_Client_Human_Server_Human/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +21,28 @@ namespace SocketAsync_Client_Human_Server_Human
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly Regex _ipAddress = new Regex("[^0-9.]");
+        private static readonly Regex _port = new Regex("[^0-9]");
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+        private static bool IsTextAllowed(string text)
+        {
+            return !_ipAddress.IsMatch(text);
+        }
+        private void TextBox_PreviewTextInput_1(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed_1(e.Text);
+        }
+        private static bool IsTextAllowed_1(string text)
+        {
+            return !_port.IsMatch(text);
         }
     }
 }
